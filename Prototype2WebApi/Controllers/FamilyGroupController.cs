7240012ca23f644f9bc1a2f192e6713f5165f93c
Lauren42706 <4270683@myuwc.ac.za>
2022-07-14 16:17:00 +0200

    Added FamilyGroupController

---
+++ Prototype2WebApi/Controllers/FamilyGroupController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prototype2WebApi.Interfaces;
+
+namespace Prototype2WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FamilyGroupController : ControllerBase
+    {
+        private readonly IPrototypeDbRepository _prototypeDbRepository;
+        public FamilyGroupController(IPrototypeDbRepository prototypeDbRepository)
+        {
+            _prototypeDbRepository = prototypeDbRepository;
+        }
+    }
+}
